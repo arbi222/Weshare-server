4cfa8c3d5997612eb56a8afed21eb35b0e0dde5b
Arbi <64432385+arbi222@users.noreply.github.com>
2025-01-14 23:06:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const chatsRoute = require("./routes/chats")
 mongoose.connect(process.env.MONGO_URL);
 
 const corsOptions = {
-  origin: ["https://wesharemedia.onrender.com"],
+  origin: ["https://wesharemedia.onrender.com", "http://localhost:5173"],
   credentials: true,
 };
 
