7a356ac44ad8eb50637b0c9b501bed46f61bc600
Arbi <arbihamolli111@gmail.com>
2025-08-29 00:06:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -297,17 +297,17 @@ io.on("connection", (socket) => {
 
 // Adding a cron job to keep my server alive because the host (Render) on free usage will make my server inactive 
 // if no request is being made in 10-15 minutes.
-const backendUrl = "https://weshare-server.onrender.com";
-const job = new cron.CronJob('*/14 * * * *', function(){
-  https.get(backendUrl, (res) => {
-    if (res.statusCode === 200){
-      console.log("server restarted")
-    }
-  }).on("error", (err) => {
-      console.log("error");
-  })
-})
-job.start();
+// const backendUrl = "https://weshare-server.onrender.com";
+// const job = new cron.CronJob('*/14 * * * *', function(){
+//   https.get(backendUrl, (res) => {
+//     if (res.statusCode === 200){
+//       console.log("server restarted")
+//     }
+//   }).on("error", (err) => {
+//       console.log("error");
+//   })
+// })
+// job.start();
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
