add4f5bfb970deea3b42bc637081c28f94419ac1
arbi222 <arbihamolli111@gmail.com>
2026-05-09 17:19:00 +0200

    updating the email sending method

--- routes/2FA.js
+++ routes/2FA.js
@@ -1,50 +1,46 @@
 const router = require("express").Router();
 const User = require("../models/User")
-const nodeMailer = require("nodemailer");
 const crypto = require('crypto');
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
 const { verifyToken } = require("../token");
 const { generateAccessToken } = require("../token");
 
-
-const transporter = nodeMailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.WESHARE_EMAIL,
-        pass: process.env.WESHARE_PASS_CODE
-    }
-})
-
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+  username: "api",
+  key: process.env.MAILGUN_SENDER_API_KEY,
+  url: "https://api.eu.mailgun.net"
+});
 
 router.post("/sendEmailLoginCode", async (req,res) => {
     const email = req.body.email;
 
-    const user = await User.findOne({username: email});
-
-    if (!user) {
-        return res.status(400).json("User with this email does not exist!")
+    try{
+        const user = await User.findOne({username: email});
+    
+        if (!user) {
+            return res.status(400).json("User with this email does not exist!")
+        }
+    
+        const authCode = crypto.randomBytes(4).toString("hex");
+        
+        user.twoFactorAuthCode = authCode;
+        await user.save();
+    
+        await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+            from: `"Weshare Support" <noreply@${process.env.MAILGUN_DOMAIN}>`,
+            to: [user.username],
+            subject: "Two-Factor-Authentication Code",
+            text: `The Authentication code to login into your Weshare account is:\n\n 
+                                       ${authCode}`
+        });
+        
+        return res.status(200).json("Authentication code sent to your email!"); 
     }
-
-    const authCode = crypto.randomBytes(4).toString("hex");
-    
-    user.twoFactorAuthCode = authCode;
-    await user.save();
-
-    const mailOptions = {
-        from: process.env.WESHARE_EMAIL,
-        to: user.username,
-        subject: 'Two-Factor-Authentication Code',
-        text: `The Authentication code to login into your Weshare account is:\n\n 
-                                   ${authCode}`
-    };
-    
-    transporter.sendMail(mailOptions, (err, response) => {
-        if (err){
-            res.status(500).json('Error sending email!');
-        }
-        else{
-            res.status(200).json("Authentication code sent to your email!");
-        }
-    })
+    catch(err){ 
+        res.status(500).json(err);
+    }
 })
 
 router.post("/verifyLoginAuthCode/:authCode", async (req,res) => {
@@ -69,44 +65,42 @@ router.post("/verifyLoginAuthCode/:authCode", async (req,res) => {
 router.post("/sendEmailCode", verifyToken, async (req,res) => {
     const email = req.body.email;
 
-    const user = await User.findOne({username: email});
-
-    if (!user) {
-        return res.status(400).json("User with this email does not exist!")
-    }
-
-    const authCode = crypto.randomBytes(4).toString("hex");
+    try{
+        const user = await User.findOne({username: email});
     
-    user.twoFactorAuthCode = authCode;
-    await user.save();
-
-    if (user.isTwoFactorAuthOn) {
-        var mailOptions = {
-            from: process.env.WESHARE_EMAIL,
-            to: user.username,
-            subject: 'Two-Factor-Authentication Deactivator',
-            text: `The Authentication code to deactivate the Two-Factor-Authentication for your Weshare app is:\n\n 
-                                       ${authCode}`
-        };
-    }
-    else{
-        var mailOptions = {
-            from: process.env.WESHARE_EMAIL,
-            to: user.username,
-            subject: 'Two-Factor-Authentication Activator',
-            text: `The Authentication code to activate the Two-Factor-Authentication for your Weshare app is:\n\n 
-                                       ${authCode}`
-        };
-    }
+        if (!user) {
+            return res.status(400).json("User with this email does not exist!")
+        }
     
-    transporter.sendMail(mailOptions, (err, response) => {
-        if (err){
-            res.status(500).json('Error sending email!');
+        const authCode = crypto.randomBytes(4).toString("hex");
+        
+        user.twoFactorAuthCode = authCode;
+        await user.save();
+    
+        if (user.isTwoFactorAuthOn) {
+            await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+                from: `"Weshare Support" <noreply@${process.env.MAILGUN_DOMAIN}>`,
+                to: [user.username],
+                subject: "Two-Factor-Authentication Deactivator",
+                text: `The Authentication code to deactivate the Two-Factor-Authentication for your Weshare account is:\n\n 
+                                           ${authCode}`
+            });
         }
         else{
-            res.status(200).json("Authentication code sent to your email!");
+            await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+                from: `"Weshare Support" <noreply@${process.env.MAILGUN_DOMAIN}>`,
+                to: [user.username],
+                subject: "Two-Factor-Authentication Activator",
+                text: `The Authentication code to activate the Two-Factor-Authentication for your Weshare app is:\n\n 
+                                           ${authCode}`
+            });
         }
-    })
+    
+        return res.status(200).json("Authentication code sent to your email!"); 
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
 })
 
 router.post("/verifyAuthCode/:authCode", verifyToken, async (req,res) => {
--- routes/resetPass.js
+++ routes/resetPass.js
@@ -1,47 +1,42 @@
 const router = require("express").Router();
-const User = require("../models/User")
-const nodeMailer = require("nodemailer");
+const User = require("../models/User");
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
 const crypto = require('crypto');
 
-
-const transporter = nodeMailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.WESHARE_EMAIL,
-        pass: process.env.WESHARE_PASS_CODE
-    }
-})
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+  username: "api",
+  key: process.env.MAILGUN_SENDER_API_KEY,
+  url: "https://api.eu.mailgun.net"
+});
 
 router.post("/forgotPassword", async (req,res) => {
     const email = req.body.email;
 
-    const user = await User.findOne({username: email});
-
-    if (!user) {
-        return res.status(400).json("User with this email does not exist!")
+    try{
+        const user = await User.findOne({username: email});
+    
+        if (!user) {
+            return res.status(400).json("User with this email does not exist!")
+        }
+    
+        const token = crypto.randomBytes(25).toString("hex");
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 600000 // 10 min
+    
+        await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+            from: `"Weshare Support" <noreply@${process.env.MAILGUN_DOMAIN}>`,
+            to: [user.username],
+            subject: "Password Reset for your Weshare account",
+            text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process within 10 minutes of receiving it:\n\nhttps://wesharemedia.onrender.com/reset-password/${token}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n`
+        });
+    
+        return res.status(200).json("Recovery email sent to your email account!"); 
     }
-
-    const token = crypto.randomBytes(25).toString("hex");
-    user.resetPasswordToken = token;
-    user.resetPasswordExpires = Date.now() + 600000 // 10 min
-
-    const mailOptions = {
-        from: process.env.WESHARE_EMAIL,
-        to: user.username,
-        subject: 'Password Reset for Weshare',
-        text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process within 10 minutes of receiving it:\n\nhttps://wesharemedia.onrender.com/reset-password/${token}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n`
-      };
-
-    transporter.sendMail(mailOptions, (err, response) => {
-        if (err){
-            console.log(err);
-            res.status(500).json('Error sending email!');
-        }
-        else{
-            user.save();
-            res.status(200).json("Recovery email sent!");
-        }
-    })
+    catch(err){
+        res.status(500).json(err);
+    }
 })
 
 
